0fbc519a5ca58c25251d7732d561290f833c7907
Lucas Ellery <olucasellery@gmail.com>
2021-12-28 16:45:00 -0300

    sass added | styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import Header from './Header';
+import Routes from './Routes';
+import { ThemeProvider } from './ThemeContext';
+import styles from './App.scss';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: Math.random(), title: "Title#01", subtitle: 'Sub#01', likes: 230, read: false, removed: false},
+    {id: Math.random(), title: "Title#02", subtitle: 'Sub#02', likes: 422, read: true, removed: false},
+    {id: Math.random(), title: "Title#03", subtitle: 'Sub#03', likes: 824, read: true, removed: false},
+    {id: Math.random(), title: "Title#04", subtitle: 'Sub#04', likes: 824, read: false, removed: false},
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: 50824
+      },
+    ]);
+  }
+
+  function handleRemovePost(postId) {
+    // setPosts((prevState) => (
+    //   prevState.filter(post => post.id !== postId)
+    // ));
+    setPosts((prevState) => prevState.map(
+      post => (
+        post.id === postId
+          ? { ...post, removed: true }
+          : post
+      )
+    ))
+  }
+
   return (
-    <>
-      <Header >
-        <h2>Posts da semana</h2>
+    <ThemeProvider>
+      <Header>
+        <h2 className={styles.title}>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
       
       <hr />
 
-      <Post
-        post={{
-          title: 'React vai ser o novo framework front-end',
-          subtitle: 'React vai ser o novo framework front-end'
-        }}
-        likes={100}
-      />
+      <Routes />
 
-      <Post
-        post={{
-          title: 'React vai ser o novo framework front-end',
-          subtitle: 'React vai ser o novo framework front-end'
-        }}
-        likes={100}
-      />
-
-      <Post
-        post={{
-          title: 'React vai ser o novo framework front-end',
-          subtitle: 'React vai ser o novo framework front-end'
-        }}
-        likes={100}
-      />
-    </>
+      {posts.map(post => (
+        <Post
+          key={post.id}
+          likes={post.likes}
+          onRemove={handleRemovePost}
+          post={post}
+        />
+      ))}
+    </ThemeProvider>
   )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
-export default function Header({ title = "JStack", children }) {
+import styles from './Header.scss';
+
+import { ThemeContext } from './ThemeContext';
+
+export default function Header(props) {
+  const { onToggleTheme } = useContext(ThemeContext);
+
   return (
-    <>
-      <h1>{title}</h1>
-      {children}
-    </>
+    <div>
+      <h1 className={styles.title}>{props.title}</h1>
+      <Button onClick={onToggleTheme}>
+        Mudar tema
+      </Button>
+      {props.children}
+    </div>
   )
 }
 
-Header.propTypes = { 
+Header.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node,
 }
