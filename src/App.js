09f5c1c9abebd6606b6b67e9a1aa1ee622bcd1d2
Lucas Ellery <olucasellery@gmail.com>
2022-01-07 13:17:17 -0300

    changin the theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
 
+import themes from './styles/themes';
+
 function App() {
+  const [theme, setTheme] = useState();
+
+
+
   return (
-    <>
+    <ThemeProvider theme={themes.light}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   )
 }
 
