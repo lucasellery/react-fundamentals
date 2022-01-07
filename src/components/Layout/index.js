02632913864bbfd0b620f97117371b2d888cae28
Lucas Ellery <olucasellery@gmail.com>
2022-01-07 10:30:01 -0300

    posts placed in components folder

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from '../Header';
-import PostsList from '../../PostsList';
+import PostsList from '../PostsList';
 import Footer from '../Footer';
 
 export default function Layout() {
